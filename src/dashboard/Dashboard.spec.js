e8c3f6667fa33934b9f9bbfa3b783f512f9cd85c
Mike Harley <mharley12345@gmail.com>
2019-09-16 14:58:39 -0700

    Controls Test Passed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,11 @@
 // Test away
+import React from "react";
+import renderer from "react-test-renderer";
+import Dashboard from "./Dashboard";
+
+describe("<Dashboard />", () => {
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Dashboard />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
